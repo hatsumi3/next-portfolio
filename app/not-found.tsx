5b29c76d4db2093fe592f4ccba36b6d592a7328a
hatsumi3 <al.usamiku39@gmail.com>
2023-06-19 16:23:01 +0900

    feat: add not found page.

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const NotFound: FC = () => {
+  return (
+    <div style={{ textAlign: "center", height: "70vh" }}>
+      <h1>404 Not Found</h1>
+      <p>ページが見つかりません。</p>
+    </div>
+  );
+};
+
+export default NotFound;
